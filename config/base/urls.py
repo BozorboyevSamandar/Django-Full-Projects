c7adc61b1b6d64f8f13fe23435e9004133a10a01
BS_CODER <92523813+BozorboyevSamandar@users.noreply.github.com>
2022-08-11 20:50:29 +0500

    add new project

---
+++ config/base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+]
