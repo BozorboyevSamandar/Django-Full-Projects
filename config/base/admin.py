e73045d3fd5f51b544e06c4d541b8ff1a5b1d38f
BozorboyevSamandar <bscoders@gmail.com>
2022-10-24 19:59:12 +0300

    add commit admin.py

--- config/base/admin.py
+++ config/base/admin.py
@@ -1,25 +1,9 @@
 from django.contrib import admin
 from .models import Room, Topic, Message
 
-
 # Register your models here.
 
-class RoomAdmin(admin.ModelAdmin):
-    list_display = ('name', 'participants', 'updated', 'created', 'views')
 
-
-admin.site.register(Room, RoomAdmin)
-
-
-class TopicAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
-admin.site.register(Topic, TopicAdmin)
-
-
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'room', 'updated', 'created')
-
-
-admin.site.register(Message, MessageAdmin)
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
